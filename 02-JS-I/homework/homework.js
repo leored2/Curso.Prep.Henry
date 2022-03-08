b3ae90e853c4db1d84f42ff81ba7d9880ad663f4
Leo Anaya <jeremiaz298@gmail.com>
2022-03-07 18:55:49 -0600

    Third

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -166,15 +166,18 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (num ==== 0 ){
+  if(numero === 0) {
     return false;
   }
-  else if (numero > 0) {
+  else if(numero > 0) {
     return "Es positivo";
-  } else {
-    return "Es negativo";
-   }
   }
+  else {
+    return "Es negativo";
+  }
+}
+
+
 
 
 function agregarSimboloExclamacion(str) {
